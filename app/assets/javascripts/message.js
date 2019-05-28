0866ab7391f2ea05545d053e112288b2c9cce50d
Takaki <takaki55730317@gmail.com>
2019-05-28 16:53:22 +0900

    message.jsのval('')ではなくreset();に変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -37,9 +37,9 @@ $(document).on('turbolinks:load', function (){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
       $('.form__submit').prop('disabled', false);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('#new_message')[0].reset();
       $('.hidden').val('');
       return false
     })
