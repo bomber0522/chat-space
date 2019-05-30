f8f819af7125f059bdd6ced86a7d8d03aac143ef
Takaki <takaki55730317@gmail.com>
2019-05-30 14:48:27 +0900

    インクリメンタルサーチ一部修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -57,8 +57,8 @@ $(document).on('turbolinks:load', function(){
   })
 
   $(document).on("click", ".user-search-add.chat-group-user__btn.chat-group-user__btn--add", function () {
-    var user_id = $(this).attr("data-user-id");
-    var user_name = $(this).attr("data-user-name");
+    var user_id = $(this).data("user-id");
+    var user_name = $(this).data("user-name");
     $(this).parent().remove();
     appendGroupUser(user_id, user_name);
   })
