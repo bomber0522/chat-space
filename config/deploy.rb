278761c8d1a1cc4cf357ffbe73122671f4a91a92
Takaki <takaki55730317@gmail.com>
2019-06-03 10:50:46 +0900

    fixed_s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ lock "~> 3.11.0"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:bomber0522/chat-space.git"
-
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
@@ -15,6 +14,13 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
