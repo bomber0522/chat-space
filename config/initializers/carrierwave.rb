2caa20eb806ca0629795c2d25cf52d717efd70f5
Takaki <takaki55730317@gmail.com>
2019-08-31 21:45:08 +0900

    modify carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,11 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
+    :provider              => 'AWS',
+    :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+    :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET_KEY']
   }
 
-  config.fog_directory  = 'pepe-takafree'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/pepe-takafree'
+  config.fog_directory     =  ENV['S3_BUCKET']
 end
